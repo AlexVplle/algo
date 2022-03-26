b9dbbafdbca2e9d20406aa9228ba113c6b90fb35
AlexVplle <alexisvapaille11@gmail.com>
2022-03-26 18:26:41 +0100

    add binary search iterative

--- src/search/binarySearch.py
+++ src/search/binarySearch.py
@@ -1,12 +1,20 @@
-def binarySearch(array : list, target : int) :
+def binarySearchRecursive(array : list, target : int) :
     """Complexity : O(log(n))"""
+    if target not in array : return -1
     end = len(array) - 1
     mid = end // 2
-    if not array : return 0
     valueMid = array[mid]
     if valueMid == target: return mid
-    if len(array) == 1 : 
-        if array[0] < target : return 1
-        return 0
-    if valueMid > target : return binarySearch(array[:mid], target)
-    else : return mid + 1 + binarySearch(array[mid+1:], target)
+    if valueMid > target : return self.search(array[:mid], target)
+    else : return mid + 1 + self.search(array[mid+1:], target)
+
+def binarySearchIterative(array : list, target : int) :
+    """Complexity : O(log(n))"""
+    start, end = 0, len(array) - 1
+    while start <= end :
+        mid = (start + end) // 2
+        valueMid = array[mid]
+        if valueMid == target : return mid
+        if valueMid > target : end = mid - 1
+        else : start = mid + 1
+    return -1
